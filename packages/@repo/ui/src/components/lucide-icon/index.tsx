64acf7c2094c6763320df9727f418e298db78a51
notcharliee <saunderscharlie@hotmail.com>
2024-12-29 13:30:14 +0000

    revert: 7776567a04bb101495ad424d4b556aa5ff76fc3b

--- packages/@repo/ui/src/components/lucide-icon/index.tsx
+++ packages/@repo/ui/src/components/lucide-icon/index.tsx
@@ -1,19 +1,43 @@
-import { lazy } from "react"
-
-import dynamicIcons from "lucide-react/dynamicIconImports.js"
+import { pascalCase } from "change-case"
+import { icons as lucideIcons } from "lucide-react"
 
 import { cn } from "~/lib/utils"
 
 import type { LucideProps } from "lucide-react"
 
-export type LucideIconName = keyof typeof dynamicIcons
+type PascalToKebab<
+  TValue extends string,
+  TAccumulator extends string = "",
+> = TValue extends `${infer First}${infer Rest}`
+  ? First extends Uppercase<First>
+    ? PascalToKebab<
+        Rest,
+        `${TAccumulator}${TAccumulator extends "" ? "" : "-"}${Lowercase<First>}`
+      >
+    : PascalToKebab<Rest, `${TAccumulator}${First}`>
+  : TAccumulator
+
+export type LucideIconPascalName = keyof typeof lucideIcons
+export type LucideIconName = PascalToKebab<LucideIconPascalName>
+
+const pascalIconNames = Object.keys(lucideIcons) as LucideIconPascalName[]
+
+function isValidIconName(name: string): name is LucideIconPascalName {
+  return pascalIconNames.includes(name as LucideIconPascalName)
+}
 
 export interface LucideIconProps extends LucideProps {
   name: LucideIconName
 }
 
 export function LucideIcon({ name, className, ...props }: LucideIconProps) {
-  const Icon = lazy(dynamicIcons[name])
+  const pascalName = pascalCase(name).replaceAll("_", "")
+
+  if (!isValidIconName(pascalName)) {
+    throw new Error(`Invalid Lucide icon name: ${pascalName}`)
+  }
+
+  const Icon = lucideIcons[pascalName]
 
   return (
     <Icon
