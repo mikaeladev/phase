f2b63c2bf5a1874ca107cf968891c689a97bd41d
notcharliee <saunderscharlie@hotmail.com>
2024-12-29 14:16:46 +0000

    feat: add a bunch of stuff to docs

--- apps/docs/src/config/sidebar.ts
+++ apps/docs/src/config/sidebar.ts
@@ -60,6 +60,27 @@ export async function getSidebarNavItems(): Promise<SidebarNavItems> {
   return [
     {
       type: "category",
+      label: "Welcome",
+      children: [
+        {
+          type: "page",
+          label: "Introduction",
+          href: "/docs",
+        },
+        {
+          type: "page",
+          label: "Terms of Service",
+          href: "/docs/terms",
+        },
+        {
+          type: "page",
+          label: "Privacy Policy",
+          href: "/docs/privacy",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "Discord Bot",
       children: parseEntries(botEntries),
     },
