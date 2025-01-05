f9956dfcf4635dfd320465a5a6d4b9328d6f5217
notcharliee <saunderscharlie@hotmail.com>
2025-01-05 08:10:07 +0000

    fix: load env variables in astro config

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,10 +1,15 @@
-import "./src/env"
-
 import { defineConfig } from "astro/config"
+import { loadEnv } from "vite"
 
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
+
+// loads environment variables
+process.env = loadEnv(process.env.NODE_ENV!, process.cwd(), "")
+
+// validates environment variables
+await import("./src/lib/env")
 
 export default defineConfig({
   base: "/docs",
