1fde4d39a7de16f091d01bc9c1425fd58f317dce
notcharliee <saunderscharlie@hotmail.com>
2025-01-13 17:39:49 +0000

    fix: use BASE_URL from @repo/env for config instead of environment variables directly

--- packages/@repo/config/site/www/index.ts
+++ packages/@repo/config/site/www/index.ts
@@ -1,8 +1,9 @@
-const baseURL = process.env.BASE_URL ?? process.env.NEXT_PUBLIC_BASE_URL
-if (!baseURL) throw new Error("BASE_URL is not set")
+import { base as baseEnv } from "@repo/env"
+
+const { BASE_URL } = baseEnv()
 
 export default {
-  url: baseURL,
+  url: BASE_URL,
   title: "Phase Bot",
   description:
     "Phase is a free to use, open source Discord bot that aims to be the all-in-one solution for as many servers as possible.",
@@ -12,8 +13,8 @@ export default {
     url: "https://github.com/mikaeladev",
   },
   images: {
-    logo: new URL("/phase.png", baseURL),
-    favicon: new URL("/favicon.ico", baseURL),
-    apple: new URL("/apple.png", baseURL),
+    logo: new URL("/phase.png", BASE_URL),
+    favicon: new URL("/favicon.ico", BASE_URL),
+    apple: new URL("/apple.png", BASE_URL),
   },
 }
