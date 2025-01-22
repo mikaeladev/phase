89ab38131093fd6d1f5e289823e597c8e7145287
notcharliee <saunderscharlie@hotmail.com>
2025-01-22 15:03:06 +0000

    refactor: restructure bot configuration

--- apps/bot/src/lib/config.ts
+++ apps/bot/src/lib/config.ts
@@ -1,6 +1,54 @@
+import { GatewayIntentBits, Options, Partials } from "discord.js"
+
+import { Emojis } from "~/lib/emojis"
+
 import { version } from "~/../package.json"
+
+import type { ClientOptions } from "discord.js"
+
+export interface BotConfig extends ClientOptions {
+  name: string
+  version: string
+}
 
 export const botConfig = {
   name: "Phase",
   version: version,
-}
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.User,
+  ],
+  sweepers: {
+    ...Options.DefaultSweeperSettings,
+    messages: {
+      interval: 60 * 60,
+      lifetime: 60 * 30,
+    },
+  },
+  makeCache: Options.cacheWithLimits({
+    ...Options.DefaultMakeCacheSettings,
+    ReactionManager: {
+      maxSize: 0,
+      keepOverLimit: (reaction) => {
+        const reactionsToKeep = [Emojis.GiveawayReaction]
+        return !!(
+          reaction.me &&
+          reaction.emoji.name &&
+          reactionsToKeep.includes(reaction.emoji.name)
+        )
+      },
+    },
+  }),
+} as const satisfies BotConfig
--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -1,7 +1,7 @@
 import { BotClient } from "@phasejs/core/client"
 import { loadApp } from "@phasejs/loaders"
 import { logsPlugin } from "@phasejs/logs"
-import { Client, GatewayIntentBits, Options, Partials } from "discord.js"
+import { Client } from "discord.js"
 
 import { blacklistPlugin } from "@plugin/blacklist"
 import { musicPlugin } from "@plugin/music"
@@ -9,7 +9,7 @@ import { voicePlugin } from "@plugin/voice"
 
 import { blacklistOptions } from "~/lib/blacklist"
 import { botConfig } from "~/lib/config"
-import { Emojis, emojiSyncPlugin } from "~/lib/emojis"
+import { emojiSyncPlugin } from "~/lib/emojis"
 import { trpcPlugin } from "~/lib/trpc"
 
 import { ConfigStore } from "~/structures/stores/ConfigStore"
@@ -18,53 +18,15 @@ import { InviteStore } from "~/structures/stores/InviteStore"
 import { StreamerStore } from "~/structures/stores/StreamerStore"
 
 // create the underlying discord.js client
-const djsClient = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildModeration,
-    GatewayIntentBits.GuildInvites,
-    GatewayIntentBits.GuildVoiceStates,
-  ],
-  partials: [
-    Partials.Channel,
-    Partials.GuildMember,
-    Partials.Message,
-    Partials.Reaction,
-    Partials.User,
-  ],
-  sweepers: {
-    ...Options.DefaultSweeperSettings,
-    messages: {
-      interval: 60 * 60,
-      lifetime: 60 * 30,
-    },
-  },
-  makeCache: Options.cacheWithLimits({
-    ...Options.DefaultMakeCacheSettings,
-    ReactionManager: {
-      maxSize: 0,
-      keepOverLimit: (reaction) => {
-        const reactionsToKeep = [Emojis.GiveawayReaction]
-        return !!(
-          reaction.me &&
-          reaction.emoji.name &&
-          reactionsToKeep.includes(reaction.emoji.name)
-        )
-      },
-    },
-  }),
-})
+const djsClient = new Client(botConfig)
 
 // create the phasejs client wrapper
 const phaseClient = new BotClient(djsClient, {
   plugins: [
     logsPlugin(botConfig),
-    blacklistPlugin(blacklistOptions),
     voicePlugin(),
     musicPlugin(),
+    blacklistPlugin(blacklistOptions),
     emojiSyncPlugin(),
     trpcPlugin(),
   ],
