2ce9212728efce4b1cb97c7c49359160fa3535a1
notcharliee <saunderscharlie@hotmail.com>
2025-01-24 17:18:25 +0000

    fix: dashboard server actions

--- apps/dash/next.config.ts
+++ apps/dash/next.config.ts
@@ -1,8 +1,15 @@
 import "~/lib/env"
 
+import www from "@repo/config/site/www/index.ts"
+
 import type { NextConfig } from "next"
 
 export default {
   transpilePackages: ["@repo/ui"],
   basePath: "/dashboard",
+  experimental: {
+    serverActions: {
+      allowedOrigins: [www.url],
+    },
+  },
 } satisfies NextConfig
