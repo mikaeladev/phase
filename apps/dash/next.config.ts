70a1492f941e39f35006062e80561e39d07fe3aa
notcharliee <saunderscharlie@hotmail.com>
2025-02-03 04:25:46 +0000

    feat: update allowedOrigins to use a getter for linting compatibility in CI environments

--- apps/dash/next.config.ts
+++ apps/dash/next.config.ts
@@ -7,7 +7,12 @@ export default {
   basePath: "/dashboard",
   experimental: {
     serverActions: {
-      allowedOrigins: [env.NEXT_PUBLIC_BASE_URL.replace(/^https?:\/\//, "")],
+      // uses getter to allow linting in CI where env is not set
+      get allowedOrigins() {
+        const protocolRegex = /^https?:\/\//
+        const proxyOrigin = env.NEXT_PUBLIC_BASE_URL.replace(protocolRegex, "")
+        return [proxyOrigin]
+      },
     },
   },
 } satisfies NextConfig
