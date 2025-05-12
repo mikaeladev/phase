07caae6af6a9287364bf8175514dbccc8bfbce74
notcharliee <saunderscharlie@hotmail.com>
2025-05-12 15:13:32 +0100

    refactor: merge some bot dts files

---
+++ apps/bot/src/types/declarations/assets.d.ts
@@ -0,0 +1,9 @@
+declare module "*.png" {
+  const path: string
+  export default path
+}
+
+declare module "*.otf" {
+  const path: string
+  export default path
+}
--- apps/bot/src/types/declarations/fonts.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.otf" {
-  const content: string
-  export default content
-}
